f6c6bd31fa3fc714fd730f3ead0c108cea57504e
pateluday07 <pateluday07@gmail.com>
2026-08-09 03:30:23 +0530

    reflection

--- src/patterns/creational/singleton/Tester.java
+++ src/patterns/creational/singleton/Tester.java
@@ -1,24 +1,37 @@
 package patterns.creational.singleton;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+
 public class Tester {
 
-    public static void main(String[] args) {
-        //memory 123
-        ConfigurationManager configManager = ConfigurationManager.getInstance();
-        configManager.addConfig();
-        System.out.println(configManager.getConfig());
+    public static void main(String[] args) throws Exception {
+        System.out.println("Singleton Pattern creating configuration manager using getInstance() method..");
 
-        //memory 123
+        //config manager one
+        ConfigurationManager configManager1 = ConfigurationManager.getInstance();
+
+        //config manager two
         ConfigurationManager configManager2 = ConfigurationManager.getInstance();
-        configManager2.addConfig();
-        System.out.println(configManager2.getConfig());
 
+        System.out.println("comparing configManager1 and configManager2 using == operator..");
+        System.out.println(configManager1 == configManager2);
+
+        //config manager three
+        System.out.println("\ncreating configManager3 using reflection by accessing the private instance field..");
+        Field instanceField = ConfigurationManager.class.getDeclaredField("instance");
+        instanceField.setAccessible(true);
+        instanceField.set(null, null); // Reset the instance field to null
         ConfigurationManager configManager3 = ConfigurationManager.getInstance();
-        ConfigurationManager configManager4 = ConfigurationManager.getInstance();
+        System.out.println("comparing configManager1 and configManager3 using == operator..");
+        System.out.println(configManager1 == configManager3);
 
-        System.out.println(configManager == configManager2);
-        System.out.println(configManager == configManager3);
-        System.out.println(configManager3 == configManager4);
-
+        //config manager four
+        System.out.println("\ncreating configManager4 using reflection by accessing the private constructor..");
+        Constructor<ConfigurationManager> constructor = ConfigurationManager.class.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        ConfigurationManager configManager4 = constructor.newInstance();
+        System.out.println("comparing configManager1 and configManager4 using == operator..");
+        System.out.println(configManager1 == configManager4);
     }
 }
